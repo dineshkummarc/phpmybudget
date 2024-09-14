a8879145a6b4ab1803a86997083e680e6666f260
Dinesh Kumar <mapmydestination@gmail.com>
2024-09-15 01:42:15 +0530

    update

--- testmysql.php
+++ testmysql.php
@@ -1,7 +1,7 @@
 <?php 
-$link = mysql_connect('hostname','dbuser','dbpassword'); 
+$link = mysqli_connect('hostname','dbuser','dbpassword'); 
 if (!$link) { 
-	die('Could not connect to MySQL: ' . mysql_error()); 
+	die('Could not connect to MySQL: ' . mysqli_error()); 
 } 
-echo 'Connection OK'; mysql_close($link); 
+echo 'Connection OK'; mysqli_close($link);
 ?> 
